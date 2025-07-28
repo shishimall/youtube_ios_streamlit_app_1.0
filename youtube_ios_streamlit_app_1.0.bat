344d8c1313b20147c5e47befafd11f98f692550c
shishimall <158637584+shishimall@users.noreply.github.com>
2025-07-28 23:21:13 +0900

    Add files via upload

--- youtube_ios_streamlit_app_1.0.bat
+++ youtube_ios_streamlit_app_1.0.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\YOUTUBE_IOS_STREAMLIT_APP_1.0"
-python "C:\YOUTUBE_IOS_STREAMLIT_APP_1.0\youtube_ios_streamlit_app_1.0.py"
+cd /d "\\MyNAS\home\YOUTUBE_IOS_STREAMLIT_APP_1.0"
+python "\\MyNAS\home\YOUTUBE_IOS_STREAMLIT_APP_1.0\youtube_ios_streamlit_app_1.0.py"
 pause
